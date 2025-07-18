06a1c41f4b1e6caab9e4e7576cf9001ba3dca7bf
kevindandrew <kevindandrew77@gmail.com>
2025-07-18 12:54:23 -0400

    ejercicio objts

--- dia-15/main.js
+++ dia-15/main.js
@@ -22,11 +22,10 @@ let primerEstudiante = nuevoArray.shift();
 
 /* METODO IMPORTANTE!!!!!! */
 let largo = nuevoArray.length;
-console.log(largo);
-console.log(nuevoArray);
+
 /* ARRAY Q VEO ARRAY Q ITERO */
 for (let index = 0; index < nuevoArray.length; index++) {
-  console.log(nuevoArray[index]);
+  /*  console.log(nuevoArray[index]); */
 }
 
 /* las notas de un estudiante de funval estn dentro de un array
@@ -37,3 +36,104 @@ let notas =[32]
 let notas =[32,77]
 realizar una funcion q reciba un array y nos retorne el promedio de las notas de un estudiante :D
 */
+
+function promedio(arrayNotas) {
+  let resultado = 0;
+  for (let i = 0; i < arrayNotas.length; i++) {
+    resultado = resultado + arrayNotas[i];
+  }
+  return resultado / arrayNotas.length;
+}
+let notas1 = [32, 100, 25, 78];
+let notas2 = [32, 100, 10, 8, 0];
+let notas3 = [32];
+let notas4 = [32, 77];
+
+/* console.log(promedio(notas1));
+console.log(promedio(notas2));
+console.log(promedio(notas3));
+console.log(promedio(notas4)); */
+/* ------------------------------------------------------ */
+
+let matriz = [
+  [28, 39, 90, 89],
+  [70, 32, 28, 18],
+  [10, 0, 15, 31],
+  [11, 23, 45, 67],
+];
+
+/* realizar la suma y mostrar el total de la diagonal principal pero usando un solo for */
+
+/* OBJETOS */
+/* DECLARACION Y CREACION DE OBJETOS */
+
+let estudiante = {
+  nombre: "kevin",
+  edad: 28,
+  nacionalidad: "Bolivia",
+  esMiembro: true,
+  numeroCuenta: 12345667897892344n,
+};
+/* como accedemos a valores dentro de los objetos */
+
+estudiante.nacionalidad = "Chile";
+/* como creamos llaves nuevas */
+estudiante.genero = "Masculino";
+
+/* --------------------------------- */
+let listaEstudiantes = [
+  {
+    nombre: "kevin",
+    edad: 28,
+    nacionalidad: "Bolivia",
+    esMiembro: true,
+    numeroCuenta: 12345667897892344n,
+    notas: [11, 23, 45, 67],
+  },
+  {
+    nombre: "Pablo",
+    edad: 33,
+    nacionalidad: "Argentina",
+    esMiembro: true,
+    numeroCuenta: 12322876454n,
+    notas: [99, 80, 100, 10],
+  },
+  {
+    nombre: "Yamila",
+    edad: 22,
+    nacionalidad: "Argentina",
+    esMiembro: true,
+    numeroCuenta: 112894789123479812n,
+    notas: [77, 99, 51, 80],
+  },
+  {
+    nombre: "Sebastian",
+    edad: 25,
+    nacionalidad: "Peru",
+    esMiembro: true,
+    numeroCuenta: 12345667897892344n,
+    notas: [0, 10, 0, 100],
+  },
+  {
+    nombre: "jeff",
+    edad: 34,
+    nacionalidad: "Peru",
+    esMiembro: true,
+    numeroCuenta: 12345667897892344n,
+    notas: [100, 90, 99, 88],
+  },
+];
+/* objetivo mostrar a los estudiantes cuyo promedio sea mayor a 65pts */
+
+for (let index = 0; index < listaEstudiantes.length; index++) {
+  let promedioEstudiante = promedio(listaEstudiantes[index].notas);
+  if (promedioEstudiante >= 65) {
+    console.log(
+      listaEstudiantes[index].nombre +
+        " este estudiante tiene un promedio de " +
+        promedioEstudiante
+    );
+  }
+}
+
+/* mostrar a todos los estudiantes q sean de Argentina */
